39e359305ecbf17083cf7950ea902214baf54e50
Swedish-li <one.piece.li.2016@gmail.com>
2020-04-07 22:49:16 +0900

    init commit

---
+++ .vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: "Swedish-li's blog",
+  description: "just My Blog",
+  plugins: ["@vuepress/blog"],
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ]
+};
