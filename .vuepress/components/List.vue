bc22d5d235645fba10c6040ecfe8ca6a38c1d20c
Swedish-li <one.piece.li.2016@gmail.com>
2020-06-15 17:04:31 +0900

    add post

---
+++ .vuepress/components/List.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="article" v-for="page in files">
+      <a v-bind:href="page.path">{{page.title}}</a>
+      <div class="keywords">
+        <span class="keyword" v-for="key in page.frontmatter.tags">{{key}}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    files() {
+      return this.$site.pages
+        .filter(p => { 
+          console.log(p);
+          return p.path.indexOf('/pages/') >= 0;
+        });
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .article {
+    margin-bottom: 20px;
+    border-left: solid 5px #26c92e;
+    padding: 20px;
+  }
+  .keywords {
+    margin-top: 10px;
+  }
+  .keyword {
+    padding: 5px;
+    border-radius: 7px;
+    font-size: small;
+    background: #3e5caf;
+    margin-right: 5px;
+    color: white;
+    font-weight: 500;
+  }
+</style>
--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,7 +2,11 @@ module.exports = {
   title: "Swedish-li's blog",
   description: "just My Blog",
   plugins: ["@vuepress/blog"],
-  head: [
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
-  ]
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+  markdown: {
+    lineNumbers: true,
+  },
+  themeConfig: {
+    logo: '/avatar.png',
+  }
 };
